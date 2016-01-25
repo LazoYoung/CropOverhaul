1a26a4ee18d2ac757d86f3628fbe48d6e26175d2
BuildTools <unconfigured@null.spigotmc.org>
2016-01-25 17:55:10 +0900

    Initial commit to upload source trees.
    CropDisease, Events class were added.

---
+++ CropDisease/src/main/java/com/naver/jupiter1390/CropDisease.java
@@ -0,0 +1,14 @@
+package com.naver.jupiter1390;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CropDisease extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+		saveDefaultConfig();
+		
+	}
+	
+}
---
+++ CropDisease/src/main/java/com/naver/jupiter1390/Events.java
@@ -0,0 +1,28 @@
+package com.naver.jupiter1390;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockGrowEvent;
+import org.bukkit.plugin.Plugin;
+
+public class Events implements Listener {
+	
+	private Plugin plugin;
+	
+	public Events(Plugin plugin) {
+		
+		this.plugin = plugin;
+		
+	}
+	
+	@EventHandler
+	public void onCropGrow(BlockGrowEvent event) {
+		
+		// config.yml 가져오기 
+		FileConfiguration config = plugin.getConfig();
+		
+		
+	}
+	
+}
